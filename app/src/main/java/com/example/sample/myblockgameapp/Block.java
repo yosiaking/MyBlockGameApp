ab32b541559234d986055c3a63f279439a7fbf40
yosiaking <yoshiaki.im@gmail.com>
2016-04-20 17:38:08 +0900

    画面の回転に対応 ※画面が回転するたびにonCreateが呼び出される。 ※そのため、画面が回転するときに状態を保存して、再度復元する処理をいれた。

--- app/src/main/java/com/example/sample/myblockgameapp/Block.java
+++ app/src/main/java/com/example/sample/myblockgameapp/Block.java
@@ -3,6 +3,7 @@ package com.example.sample.myblockgameapp;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Bundle;
 
 /**
  * Created by apple on 2016/04/19.
@@ -14,6 +15,10 @@ public class Block implements DrawableItem {
     private final float mBottom;
     private final float mRight;
     private int mHard;
+    private boolean mIsCollision = false; //衝突状態を記録するフラグ
+    private boolean mIsExist = true; //ブロックが破壊されていないか// ;
+    private static final String KEY_HARD = "hard";
+
 
     public Block(float top, float left, float right, float bottom){
         this.mTop = top;
@@ -24,7 +29,23 @@ public class Block implements DrawableItem {
     }
 
     public void draw(Canvas canvas, Paint paint){
-        if(mHard > 0){
+
+        //耐久力が0以上の場合のみ
+        if(mIsExist){
+
+            //当たり判定
+            if(mIsCollision){
+                mHard--;
+                mIsCollision = false;
+
+                //耐久率が0の場合、処理終わり
+                if(mHard <= 0){
+                    mIsExist = false;
+                    return;
+                }
+            }
+
+
             //塗りつぶし部分を描画
             paint.setColor(Color.parseColor("#4444ff"));
             paint.setStyle(Paint.Style.FILL);
@@ -33,8 +54,35 @@ public class Block implements DrawableItem {
             paint.setColor(Color.parseColor("#000000"));
             paint.setStyle(Paint.Style.STROKE);
             canvas.drawRect(mLeft, mTop, mRight, mBottom, paint);
-
         }
+
+    }
+
+    //状態を保存
+    public Bundle save(){
+        Bundle outState = new Bundle();
+        outState.putInt(KEY_HARD, mHard);
+        return outState;
+    }
+
+    //Bundleから状況を復元する
+    public void restore(Bundle inState){
+        mHard = inState.getInt(KEY_HARD);
+        mIsExist = mHard > 0;
+//        if(mHard > 0){
+//            mIsExist = true;
+//        }else{
+//            mIsExist = false;
+//        }
+        //        以下の様に書き換えも可能
+    }
+
+    public void collision(){
+        mIsCollision = true; //衝突したかどうかの判定を行い、実際の破壊はdraw()で行う
+    }
+
+    public boolean isExist(){
+        return mIsExist;
     }
 
 }
